370fe46836e563dc635440c08c333ecd8d9e1456
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2025-10-29 07:16:26 +0900

    feat: nix-darwin flake module

--- modules/flake/default.nix
+++ modules/flake/default.nix
@@ -1,6 +1,6 @@
 localFlake: flakeArgs:
 builtins.foldl' (acc: elem: acc // elem) { } (
-  builtins.map (module: import ./. + "/${module}" localFlake) (
+  builtins.map (module: import (./. + "/${module}") localFlake flakeArgs) (
     builtins.filter (x: x != "default.nix") (builtins.attrNames (builtins.readDir ./.))
   )
 )

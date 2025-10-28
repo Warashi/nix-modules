bd068d78b3bf1c4db2221221427fc9ceeda7d98b
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2025-10-28 22:36:14 +0900

    feat: add initial flake.nix and flake.lock

---
+++ flake.nix
@@ -0,0 +1,127 @@
+{
+  description = "My Nix modules";
+
+  inputs = {
+    # keep-sorted start block=yes
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+    };
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs = {
+        nixpkgs-lib.follows = "nixpkgs-lib";
+      };
+    };
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs = {
+        flake-compat.follows = "flake-compat";
+        gitignore.follows = "gitignore";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+    nixpkgs-lib = {
+      url = "github:nix-community/nixpkgs.lib";
+    };
+    systems = {
+      url = "github:nix-systems/default";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    # keep-sorted end
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      imports = [
+        # To import an internal flake module: ./other.nix
+        # To import an external flake module:
+        #   1. Add foo to inputs
+        #   2. Add foo as a parameter to the outputs function
+        #   3. Add here: foo.flakeModule
+        inputs.git-hooks.flakeModule
+        inputs.treefmt-nix.flakeModule
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          # Per-system attributes can be defined here. The self' and inputs'
+          # module parameters provide easy access to attributes of the same
+          # system.
+
+          pre-commit = {
+            check.enable = true;
+            settings = {
+              src = ./.;
+              hooks = {
+                actionlint.enable = false;
+                treefmt.enable = true;
+              };
+            };
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              # keep-sorted start
+              keep-sorted.enable = true;
+              nixfmt.enable = true;
+              shfmt.enable = true;
+              # keep-sorted end
+            };
+            settings = {
+              formatter = {
+                tombi = {
+                  command = pkgs.lib.getExe pkgs.tombi;
+                  options = [ "format" ];
+                  includes = [ "*.toml" ];
+                };
+                nixfmt = {
+                  excludes = [
+                    "**/node2nix/*.nix" # node2nix generated files
+                    "**/_sources/generated.nix" # nvfetcher generatee sources
+                  ];
+                };
+              };
+            };
+          };
+
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              # keep-sorted start
+              # keep-sorted end
+            ];
+            shellHook = config.pre-commit.installationScript;
+          };
+        };
+
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+
+      };
+    };
+}
